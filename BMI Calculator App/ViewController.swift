058c303ee73b2ff21a0ad4a23b187c375cc9a1a3
Alan <csuen2@my.centennialcollege.ca>
2022-12-13 03:54:36 -0500

    Create the tab bar controller and the two screen

--- BMI Calculator App/ViewController.swift
+++ BMI Calculator App/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
